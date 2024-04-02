1e97aefa2359832fd582eacf17ebaa946a2da3cf
Sahil Garg <sahil.garg@crosslynxus.com>
2024-04-02 17:58:13 +0530

    testing

--- descriptors.py
+++ descriptors.py
@@ -1,11 +1,12 @@
 # descriptors.py
-class Verbose_attribute():
+class Verbose_attribute:
 
-    def __get__(self, obj, type=None) -> object:
+    def __get__(self, obj, type=None):
         print("accessing the attribute to get the value")
+        print(repr(obj))
         return 42
     
-    def __set__(self, obj, value) -> None:
+    def __set__(self, obj, value):
         print("accessing the attribute to set the value")
         raise AttributeError("Cannot change the value")
 
@@ -13,9 +14,5 @@ class Foo():
     attribute1 = Verbose_attribute()
 
 my_foo_object = Foo()
-x = my_foo_object.attribute1
+x = my_foo_object.attribute1  # when using dot(.) on an object, __get__() method gets called
 print(x)
-
-property()
-
-function()
--- sockets.py
+++ sockets.py
@@ -3,15 +3,18 @@ import requests
 from ip2geotools.databases.noncommercial import DbIpCity
 from geopy.distance import distance
 
-website = input("Enter Website")
-ip_address = socket.gethostbyname(website)
+# website = input("Enter Website")
+# ip_address = socket.gethostbyname(website)
 
 
-def printDetails(ip):
-    res = DbIpCity.get(ip, api_key="free")
-    print(f"IP Address: {res.ip_address}")
-    print(f"Location: {res.city}, {res.region}, {res.country}")
-    print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
+# def printDetails(ip):
+#     res = DbIpCity.get(ip, api_key="free")
+#     print(f"IP Address: {res.ip_address}")
+#     print(f"Location: {res.city}, {res.region}, {res.country}")
+#     print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
 
 
-printDetails(ip_address)
+# printDetails(ip_address)
+
+print("Socket Module", dir(socket))
+print("Request Module", dir(requests))
