32674f8e7224b7e69843e62354f1bd3dadc8d62e
Sahil Garg <sahil.garg@crosslynxus.com>
2024-04-29 16:13:10 +0530

    tesing

--- testing.py
+++ testing.py
@@ -1,285 +1,50 @@
-{
-    "routes": [
-        {
-            "id": "518ad1ed-b01c-4572-9480-6f308a39a31a",
-            "sections": [
-                {
-                    "id": "a658d21b-808b-4a3d-8a2b-579b7d818196",
-                    "type": "vehicle",
-                    "actions": [
-                        {
-                            "action": "depart",
-                            "duration": 30,
-                            "length": 30,
-                            "instruction": "Head east. Go for 30 m.",
-                            "offset": 0
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 26,
-                            "length": 37,
-                            "instruction": "Turn right toward DR Zakir Hussain Marg. Go for 37 m.",
-                            "offset": 1,
-                            "direction": "right",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 174,
-                            "length": 991,
-                            "instruction": "Turn left onto DR Zakir Hussain Marg. Go for 991 m.",
-                            "offset": 3,
-                            "direction": "left",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 144,
-                            "length": 759,
-                            "instruction": "Turn right onto Shaheed Major Mohit Sharma Marg. Go for 759 m.",
-                            "offset": 30,
-                            "direction": "right",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "roundaboutExit",
-                            "duration": 477,
-                            "length": 4143,
-                            "instruction": "Take the 1st exit from roundabout onto G T Road (NH-9) toward Vaishali/Kaushambi/Anand Vihar/U.P. Gate/Delhi/Meerut/Hapur. Go for 4.1 km.",
-                            "offset": 53,
-                            "direction": "left",
-                            "exit": 1
-                        },
-                        {
-                            "action": "keep",
-                            "duration": 117,
-                            "length": 1082,
-                            "instruction": "Keep left onto G T Road. Go for 1.1 km.",
-                            "offset": 164,
-                            "direction": "left"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 38,
-                            "length": 30,
-                            "instruction": "Turn slightly right toward Pt Deen Dayal Upadhyay Marg. Go for 30 m.",
-                            "offset": 188,
-                            "direction": "right",
-                            "severity": "light"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 313,
-                            "length": 3627,
-                            "instruction": "Turn sharp right onto Pt Deen Dayal Upadhyay Marg. Go for 3.6 km.",
-                            "offset": 191,
-                            "direction": "right",
-                            "severity": "heavy"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 42,
-                            "length": 172,
-                            "instruction": "Turn left onto Service Road. Go for 172 m.",
-                            "offset": 268,
-                            "direction": "left",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 59,
-                            "length": 70,
-                            "instruction": "Turn right toward Service Road. Go for 70 m.",
-                            "offset": 271,
-                            "direction": "right",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 125,
-                            "length": 1211,
-                            "instruction": "Turn right onto Service Road. Go for 1.2 km.",
-                            "offset": 276,
-                            "direction": "right",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "ramp",
-                            "duration": 57,
-                            "length": 807,
-                            "instruction": "Take ramp onto Service Road. Go for 807 m.",
-                            "offset": 288
-                        },
-                        {
-                            "action": "keep",
-                            "duration": 103,
-                            "length": 1398,
-                            "instruction": "Keep right onto Service Road toward Vasundhara/Indirapuram. Go for 1.4 km.",
-                            "offset": 304,
-                            "direction": "right"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 18,
-                            "length": 46,
-                            "instruction": "Turn left toward Service Road. Go for 46 m.",
-                            "offset": 337,
-                            "direction": "left",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "roundaboutExit",
-                            "duration": 57,
-                            "length": 301,
-                            "instruction": "Take the 4th exit from Expo Centre Chowk roundabout onto Service Road toward Noida City Centre. Go for 301 m.",
-                            "offset": 341,
-                            "direction": "left",
-                            "exit": 4
-                        },
-                        {
-                            "action": "ramp",
-                            "duration": 32,
-                            "length": 310,
-                            "instruction": "Take ramp onto Shaheed Hawaldar Padam Singh Dharma Marg. Go for 310 m.",
-                            "offset": 359
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 17,
-                            "length": 172,
-                            "instruction": "Continue on Vishwakarma Road. Go for 172 m.",
-                            "offset": 368
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 2,
-                            "length": 18,
-                            "instruction": "Continue on Shaheed Hawaldar Padam Singh Dharma Marg. Go for 18 m.",
-                            "offset": 373
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 3,
-                            "length": 32,
-                            "instruction": "Continue on Vishwakarma Road. Go for 32 m.",
-                            "offset": 374
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 84,
-                            "length": 865,
-                            "instruction": "Continue on Shaheed Hawaldar Padam Singh Dharma Marg. Go for 865 m.",
-                            "offset": 375
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 4,
-                            "length": 43,
-                            "instruction": "Continue on Vishwakarma Road. Go for 43 m.",
-                            "offset": 392
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 126,
-                            "length": 1333,
-                            "instruction": "Continue on Shaheed Hawaldar Padam Singh Dharma Marg. Go for 1.3 km.",
-                            "offset": 393
-                        },
-                        {
-                            "action": "ramp",
-                            "duration": 38,
-                            "length": 321,
-                            "instruction": "Take ramp onto Service Road toward Sector 66to70/Sector-18,24,To28,30,To33&53/DND Expressway. Go for 321 m.",
-                            "offset": 422,
-                            "direction": "left"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 11,
-                            "length": 29,
-                            "instruction": "Turn left onto Service Road. Go for 29 m.",
-                            "offset": 429,
-                            "direction": "left",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "continue",
-                            "duration": 116,
-                            "length": 874,
-                            "instruction": "Continue on Mamura Main Road. Go for 874 m.",
-                            "offset": 431
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 21,
-                            "length": 62,
-                            "instruction": "Turn left. Go for 62 m.",
-                            "offset": 450,
-                            "direction": "left",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "turn",
-                            "duration": 63,
-                            "length": 225,
-                            "instruction": "Turn left. Go for 225 m.",
-                            "offset": 452,
-                            "direction": "left",
-                            "severity": "quite"
-                        },
-                        {
-                            "action": "arrive",
-                            "duration": 0,
-                            "length": 0,
-                            "instruction": "Arrive at your destination on the right.",
-                            "offset": 457
-                        }
-                    ],
-                    "departure": {
-                        "time": "2021-11-01T10:00:00+05:30",
-                        "place": {
-                            "type": "place",
-                            "location": {
-                                "lat": 28.6847779,
-                                "lng": 77.3481228
-                            },
-                            "originalLocation": {
-                                "lat": 28.684669,
-                                "lng": 77.348086
-                            }
-                        }
-                    },
-                    "arrival": {
-                        "time": "2021-11-01T10:38:17+05:30",
-                        "place": {
-                            "type": "place",
-                            "location": {
-                                "lat": 28.6019875,
-                                "lng": 77.3800548
-                            },
-                            "originalLocation": {
-                                "lat": 28.601974,
-                                "lng": 77.3802649
-                            }
-                        }
-                    },
-                    "summary": {
-                        "duration": 2297,
-                        "length": 18988,
-                        "baseDuration": 2297
-                    },
-                    "polyline": "BG0-422B2x-wzE7EySrJzFrJrEzFgerE4hBrEgjBnBsJjD4c3DwgB7BsT7BgPnBsOTsJT0KnBsOnB0ZT0PUgyB8BsiBwCouB0F8zBU8GoBsJoBwHwCwMkDwRkD4SwH4mBgFoawCwM8B8G_OwC7a4DzPkDjN4DvgB0KnasJ7pB4NrJ4DzFwC_EoBjX0FnVsEjrB8G3c4D3IoB3hB4DrJoBvRwC3NwC3IoBnGoB7GoB7BwCvC8BjDUoBgyBoB4XkDw-BUofUwHAsJUkIU4I8BwgBUsOU8L8BozBoBkmBoBoa4DkpCkD4uC8Bg3BUkNoBwWsEouBwCsYoB08BTsOoBsTU4IA0FA4IUkN8BopBoB0UoBsdUgKoBgtB8B4rBoBgeoB8aoBwWUgKA0KwCofU4NUoQwCsYUsJUgPAkXAwHA0eTkNA4NToLT4XnBoVAgKTkIAsTTkcT8V7B41BAkX7Bk1BTkSnBoVvC0UvCsTvC8QrEoVrE8Q7L8pBvH4c7G0Z7B4I7B8GrEwRnB0FrEwR_EoQzFsTzFsTjD0K7BkIvMopBzUoxC7GsYnGkXjD0KnLopB3DgPvCgKvH4cvC0K3DsO3D8Q3I4hBnB4I7B8QvHkcjDgKzF0UzFsT3D0PvMwvBrEsTjNg8BvHkcvC8LvC4I7BoGjIsiB7BwH_T0rCzF4SvC4I7GsYzF4SvM4rB_EsOjD0KnQ8zBnL8kBvHsY3SoiCnLgtB3Nw5B_EoV_EsTrEoQvCkIvH4cvC4NT8GTwHjD0FjDsE_EsEoBjITjInB7GnB3DvCrEvMvMnG_ErJ_EnQnGvRzF3hB_JvW7G7VnG_O3D_YzFvRjD_J7B34C_OjSjDjS3D7Q3D3NjDnQrEjS_E_TzFr2B3S7nC_Y_Y7GnQrEvMjDnL7BzKnBjSTjNA_OoBzPwC7L8BzPsE7sCkXnG8B7LwCnLwC7QkD3S8B3SU_OTjNnB3InB3InBjInB_nB3I7jEvbr7B3N3NvCrJ7BvH7B_Y_EryDzUjlE3c_7BjN36BvMjI7Bn2CvRjuCnQ7VrErTrEnQjD7QvCvRvCzU7B_OnBzPAnQA7QU3NUnQ8BvM8BwCkIkI4XkXonC7GkD_E8BjN0FrEwCvHkDjcz_CrgCrsGjX7nCrT_7B7LjmB3D7LrE3NzF3SjInana_0CrdngD7B7GnBnGArEoB_ErE_O7a7xC3DvM_OvvBjhBzpDrO7uB_JriB3I3c3DvMzFvRvCvHjI_YzKriB_Tn9BvWnnC3Sr7B7L7kBzFrTnGzU3DjNvC_J3DrOrEvRjD3NjD_OjD_OjDrOvCrOvCnQvCvRnB_J7B7Q7BnQnBrOnB7QnBvRTvRArTUj_BA_YArnBAvbU3XAvRoBv3CA3cnGrE_E3D_EjDnG3DjDwCjD8BrE8BjDU_EA3DTrEnB3DnBjD7B3DvCvCvCvCjD3SAzUTvRT_TTvlBT7GnBrJzFjNT_dT3ST_EAvHTzUT3SAzyBTvRAzoBrE3NnB7QnBvHT_JAjSTnQ7BjInBnQ7BriBjDzUnBj6BjDzPAvHTrqC_Er7B7GnQ7B7GnB_O7B7zBzFzKnB_T7B31B_ErY7B_O7B_sB_EnGT3ITnlDrJna7BnLTzjBrE77CzKrd3DjITjIT7pB3D79BjD3NU7GTzKT_JnBrJnB3IAvWnBzPAvHAzoB7B_JA_nBvCjc7BvWTnLTrEkDnG8BzFUj6BjD7kB7Bj1B3DrOA3DwCjIgKnBoVToQT4mBnBgtBvCozBTgUAsOTwMnBk1BTsT3DkzCnBofT4NT8GTkXA0yBU4mBAkNA0F0FAsdU4Dr7B4D3wBoBvC8B7BkamC",
-                    "spans": [
-                        {
-                            "offset": 0
-                        }
-                    ],
-                    "language": "en-us",
-                    "transport": {
-                        "mode": "truck"
-                    }
-                }
-            ]
-        }
-    ]
-}
+import functools
+
+MAGIC = "abAB01"
+
+
+def new_mask(mask: int, c: str) -> int:
+    if c in MAGIC.ascii_lowercase:
+        return mask | 1
+    if c in MAGIC.ascii_uppercase:
+        return mask | 2
+    return mask | 4
+
+
+def new_repeat(repeat: str, c: str) -> str:
+    if not repeat or repeat[-1] != c:
+        return c
+    return repeat + c
+
+
+class Solution:
+    def strongPasswordChecker(self, password: str) -> int:
+        mx = 70
+
+        @functools.cache
+        def dp(position: int, length: int, mask: int, repeat: str) -> int:
+            if length > 20 or len(repeat) >= 3:
+                return mx
+
+            if position == len(password):
+                if 6 <= length and mask == 0b111:
+                    return 0
+                return mx
+
+            # no change
+            ans = dp(position + 1, length + 1, new_mask(mask, password[position]), new_repeat(repeat, password[position]))
+
+            # insert
+            for c in MAGIC:
+                ans = min(ans, 1 + dp(position, length + 1, new_mask(mask, c), new_repeat(repeat, c)))
+
+            # delete
+            ans = min(ans, 1 + dp(position + 1, length, mask, repeat))
+
+            # replace
+            for c in MAGIC:
+                ans = min(ans, 1 + dp(position + 1, length + 1, new_mask(mask, c), new_repeat(repeat, c)))
+
+            return ans
+
+        return dp(0, 0, 0, '')
